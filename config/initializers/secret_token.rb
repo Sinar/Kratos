8d03d9f4c05ff3109e0c2a63b162e1c8c5e859da
yclian <foss@yclian.com>
2011-11-08 09:21:15 +0800

    Rails 3.1.1 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kratos::Application.config.secret_token = '6af94cad3b1438f51babc1b19a3a0ba8f31d2e64a51dd6b42e22b942176d9427117f85526bb51592d32070c1f0ac6574213f53c6510d9c29cf3382f3e4ef6507'
