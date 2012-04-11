e5c1e60eac48776cbc588c9866850e40dcde08a8
yclian <foss@yclian.com>
2012-04-11 16:23:53 +0800

    Make default seeded user an admin. [#24799001]

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,8 @@
 # See: http://stackoverflow.com/questions/2708417/creating-an-admin-user-in-devise-on-rails-beta-3
 User.create({
   email: 'sinar-project@googlegroups.com',
-  password: 'What is rational is actual, what is actual is rational.'
+  password: 'What is rational is actual, what is actual is rational.',
+  role: :admin
 })
 
 # Scraped data
