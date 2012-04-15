ffece8649106d8acbc551ffb22df09f7a0c9e82e
yclian <foss@yclian.com>
2012-04-15 18:34:15 +0800

    Improve validations on models and their attributes with ActiveRecord::Validations and validatious. [#27821313]

--- app/models/coalition.rb
+++ app/models/coalition.rb
@@ -8,9 +8,12 @@ class Coalition < ActiveRecord::Base
   has_many :coalitionships, :primary_key => 'uuid', :foreign_key => 'coalition_uuid'
   has_many :parties, :through => :coalitionships
 
-  validates_uniqueness_of :uuid
-  validates_numericality_of :founded_in, :greater_than_or_equal_to => 1900, :less_than => 2100, :allow_nil => false
-  validates_numericality_of :disbanded_in, :greater_than_or_equal_to => 1900, :less_than => 2100, :allow_nil => true
+  validates :uuid, :uniqueness => true
+  validates :code, :length => { :minimum => 1, :maximum => 16 }, :uniqueness => { :scope => :disbanded_in }
+  validates :name, :length => { :minimum => 1, :maximum => 255 }
+  validates :name_in_malay, :length => { :minimum => 1, :maximum => 255 }
+  validates :founded_in, :numericality => { :greater_than_or_equal_to => 1900, :less_than => 2100 }
+  validates :disbanded_in, :numericality => { :greater_than_or_equal_to => 1900, :less_than => 2100 }, :allow_nil => true
 
   def to_param
     uuid
--- app/models/party.rb
+++ app/models/party.rb
@@ -7,9 +7,11 @@ class Party < ActiveRecord::Base
 
   has_many :coalitionships, :primary_key => 'uuid', :foreign_key => 'party_uuid'
 
-  validates_uniqueness_of :uuid
-  validates_numericality_of :founded_in, :greater_than_or_equal_to => 1900, :less_than => 2100, :allow_nil => false
-  validates_numericality_of :disbanded_in, :greater_than_or_equal_to => 1900, :less_than => 2100, :allow_nil => true
+  validates :uuid, :uniqueness => true
+  validates :name, :length => { :minimum => 1, :maximum => 255 }
+  validates :name_in_malay, :length => { :minimum => 1, :maximum => 255 }
+  validates :founded_in, :numericality => { :greater_than_or_equal_to => 1900, :less_than => 2100 }
+  validates :disbanded_in, :numericality => { :greater_than_or_equal_to => 1900, :less_than => 2100 }, :allow_nil => true
 
   def to_param
     uuid
