ab4671438fd0f712f1c9f49449163ac890a20dcb
yclian <foss@yclian.com>
2012-04-19 22:14:36 +0800

    Fix authentication token generation bug, at the view and model validation. [Fixes #28230323]

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :lockable, :rememberable, :timeoutable, :token_authenticatable, :trackable
   enum_attr :role, %w(admin basic anonymous)
 
-  validates_uniqueness_of :uuid
+  validates :uuid, :uniqueness => true
   validates :email, :presence => true, :uniqueness => true, :email => true
   validates_length_of :password, :presence => true, :in => 4..32, :allow_blank => false
 
